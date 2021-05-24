b2c4a91467761c9642e186cfc8812bf73b02dfd9
qudgus1984 <qudgus1984@naver.com>
2021-05-25 01:27:46 +0900

    message

--- django - 22/firstproject/firstapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def welcome(request):
-    return render(request, "welcome.html")
-
-def hello(request):
-    userName = request.GET['name']
-    return render(request, 'hello.html',{'userName' : userName})
