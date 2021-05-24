a9cdded8e7f333a2707a59d1f8eb4e9e0f0e9e9f
qudgus1984 <qudgus1984@naver.com>
2021-05-25 01:24:07 +0900

    message

--- django - 22/firstproject/firstproject/settings.py
+++ django - 22/firstproject/firstproject/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import my_settings
 
-DATABASES = my_settings.DATABASES
-SECRET_KEY = my_settings.SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'firstapp.apps.FirstappConfig',
-    'wordCount.apps.WordcountConfig',
 ]
 
 MIDDLEWARE = [
--- django - 22/firstproject/firstproject/urls.py
+++ django - 22/firstproject/firstproject/urls.py
@@ -13,16 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from firstproject.wordCount.views import result
 from django.contrib import admin
 from django.urls import path
-from firstapp import views as first
-from wordCount import views as wc
+from firstapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',first.welcome, name="welcome"),
-    path('hello/',first.hello, name="hello"),
-    path('wc/',wc.home,name="wc"),
-    path('wc/result/'wc.result, name="result"),
+    path('',views.welcome, name="welcome"),
+    path('hello/',views.hello, name="hello"),
 ]
