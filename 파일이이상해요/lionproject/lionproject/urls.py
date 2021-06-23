37b4633c78141f469772a07a0c477107e14a360a
HYERINI <rinlee2001@naver.com>
2021-06-23 23:40:19 +0900

    django

---
+++ 파일이이상해요/lionproject/lionproject/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from blog.views import home
+
+urlpatterns = {
+    path('admin/', admin.site.urls),
+    path('', home, name="home"),
+    path('blog/', include('blog.urls'))
+}
